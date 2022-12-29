747a2ec54f61dd501683102c24ece50e47a3666b
Princeton J. Rose <princetonjrose@gmail.com>
2022-12-29 15:49:44 -0500

    fininsed lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,19 @@
 # your code goes here!
+class Anagram:
+    pass
+    def __init__( self, word ):
+        self.word = word
+
+    def match( self, words ):
+        pass
+        anagrams = []
+        split_word = list( self.word )
+        split_word.sort()
+        for word in words:
+            w = list( word )
+            w.sort()
+            if w == split_word:
+                anagrams.append( word )
+        if anagrams:
+            return anagrams
+        else: return []
